c5f1c0d386a580593534e6e1f2ae28fa059e2cd5
akshaykoni174285 <akshaykoni174285@gmail.com>
2021-09-27 09:17:21 +0530

    updated ui

--- lib/widgets/List_of_Task.dart
+++ lib/widgets/List_of_Task.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
 
-class List_of_Task extends StatelessWidget {
+class List_of_Task extends StatefulWidget {
+  @override
+  State<List_of_Task> createState() => _List_of_TaskState();
+}
+
+class _List_of_TaskState extends State<List_of_Task> {
+  bool? ispressed = false;
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text('this is a task'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (newvalue) {},
-      ),
+      trailing: ListCheckbox(),
     );
+  }
+}
+
+class ListCheckbox extends StatelessWidget {
+  final bool checkboxstate;
+  ListCheckbox(this.checkboxstate);
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+        value: checkboxstate,
+        activeColor: Colors.lightBlueAccent,
+        // checkColor: Colors.lightBlueAccent,
+        onChanged: (value) {
+          // print('pressed');
+          // setState(() {
+          //   ispressed = value;
+          // });
+        });
   }
 }
